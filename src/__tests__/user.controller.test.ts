8ce0ba6f989fbdb667705d9dc67ce938b64a856e
Ahmad Ali <ahmad.ali9289@gmail.com>
2024-05-31 09:55:42 +0100

    - initial structure commit

---
+++ src/__tests__/user.controller.test.ts
@@ -0,0 +1,11 @@
+describle ('Vehicle Controller', () => {
+  // get the vehicle
+    // - get all vehicles
+
+    jest.mock
+
+    // - get a vehicle by id
+  // post the vehicle
+  //put the vehicle
+  //delete the vehicle
+}
